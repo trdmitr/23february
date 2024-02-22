586cb5897b31b82e5cfce043d6a0f96ac67578ff
trdmitr <trdmitr@gmail.com>
2024-02-22 21:54:54 +0400

    Updates

---
+++ precache-manifest.8bb7650ed6a165a525c2eca7f98368d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13b53326817b49490179",
+    "url": "/23february/static/css/main.b44d65a9.chunk.css"
+  },
+  {
+    "revision": "13b53326817b49490179",
+    "url": "/23february/static/js/main.e8dccc11.chunk.js"
+  },
+  {
+    "revision": "c26ba629fd600c014632",
+    "url": "/23february/static/js/runtime~main.57d26b29.js"
+  },
+  {
+    "revision": "611cbfee724dfc580040",
+    "url": "/23february/static/css/2.d3554d0c.chunk.css"
+  },
+  {
+    "revision": "611cbfee724dfc580040",
+    "url": "/23february/static/js/2.86a47bae.chunk.js"
+  },
+  {
+    "revision": "f357f7882a4bd5bcec5f",
+    "url": "/23february/static/js/3.37352ce7.chunk.js"
+  },
+  {
+    "revision": "e0df83dfd120abdfc7a63460862e9f8d",
+    "url": "/23february/static/media/Peter_Ivanovitsch.e0df83df.ttf"
+  },
+  {
+    "revision": "2a54643b24270849725450e8a1e6f41d",
+    "url": "/23february/index.html"
+  }
+];
