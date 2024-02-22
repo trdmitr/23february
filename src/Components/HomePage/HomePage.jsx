3c894a0446eb5348cb2cbfc2bf81e273788f21fb
trdmitr <trdmitr@gmail.com>
2024-02-22 06:02:40 +0400

    color

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 
 export const HomePage = () => {
-    // const imgUrl = "https://sun9-45.userapi.com/impg/CIqfClbDC5izZxbz6TYaLmPd_Ag6g2_9fB_fqg/n6izRXxtT1A.jpg?size=733x730&quality=96&sign=5167fc50b0035710e75145b572af12cd&type=album"
-    // const imgUrl2 = 'https://sun2-22.userapi.com/impg/2YiE2IW9r0friRMzAEniG_oR87ffPUfqKRswKw/vtMiX4nUf8o.jpg?size=300x300&quality=96&sign=7757c3bd97980caf5674faa89f7781c3&type=album'
-    // const imgAlt = "Каверы Подземки"
+    const imgUrl = "https://trv-axtuba.ru/wp-content/uploads/2024/02/23-fevralya.jpg"
+    // const imgUrl2 = 'https://trv-axtuba.ru/wp-content/uploads/2024/02/23-fevralya.jpg'
+    const imgAlt = "Каверы Подземки"
     
     // const frontAudio = "https://drive.google.com/uc?export=download&id=1-vJh40iIqi15VGpAAaqHJuiPZ6RA6HiI";
     // const imgTzi = "https://drive.google.com/uc?export=download&id=1gsY4lPUU4pJ07s0JwDBeJjUYddkAuSr3"
@@ -16,7 +16,7 @@ export const HomePage = () => {
               <div className="device-content">
 
                   <div>
-                      {/* <span className={classes.navLink}>Сборник за 3 года</span> */}
+                      <span className={classes.navLink}>Поздравления девушек!</span>
                       {/* <Link to="/cavers21"  className={classes.navLink21}>2021</Link>
                       <Link to="/cavers22"  className={classes.navLink22}>2022</Link> */}
                       <Link to="/cavers23"  className={classes.navLink23}>Слушаем!</Link>
@@ -26,8 +26,8 @@ export const HomePage = () => {
                       <Link to={`/cavers${yearTrib23}`} className={classes.navLink23}>2023</Link> */}
                       <div className={classes.main}>
                       
-                          <div className={classes.article}><p >С НОВЫМ 2024 ГОДОМ!</p></div>
-                          {/* <div className={classes.article}><img fetchpriority="high" role="presentation"  src={imgUrl} width={100} alt={imgAlt} /></div> */}
+                          {/* <div className={classes.article}><p >С НОВЫМ 2024 ГОДОМ!</p></div> */}
+                          <div className={classes.article}><img fetchpriority="high" role="presentation"  src={imgUrl} width={100} alt={imgAlt} /></div>
                           {/* <p className={classes.nameAudio}>Медоманы. Время Луны</p> */}
                           {/* <audio controls 
           src={frontAudio} type="audio/mpeg" /> */}
